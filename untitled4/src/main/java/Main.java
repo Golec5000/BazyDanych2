aed09d80de0512e15a475f1f58131ff8603bb547
Golec5000 <91395066+Golec5000@users.noreply.github.com>
2024-01-14 12:40:12 +0100

    Add files via upload

--- untitled4/src/main/java/Main.java
+++ untitled4/src/main/java/Main.java
@@ -1,12 +1,21 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class Main extends Application{
 
     @Override
-    public void start(Stage primaryStage) {
-        MyGui myGui = new MyGui(primaryStage);
-        myGui.showStage();
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("login-page.fxml")));
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("Bazy danych - projekt");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
