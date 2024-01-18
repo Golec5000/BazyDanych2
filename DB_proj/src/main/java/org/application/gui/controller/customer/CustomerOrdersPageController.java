44afd5f20b7a879c2b08ff3f579084d2e097e50e
Golec5000 <91395066+Golec5000@users.noreply.github.com>
2024-01-18 13:09:01 +0100

    Add files via upload

--- DB_proj/src/main/java/org/application/gui/controller/customer/CustomerOrdersPageController.java
+++ DB_proj/src/main/java/org/application/gui/controller/customer/CustomerOrdersPageController.java
@@ -1,21 +1,26 @@
 package org.application.gui.controller.customer;
 
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.*;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import org.application.entity.Order;
 import org.application.intefaces.ControllerInterface;
 import org.application.services.OrderZapytania;
 
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.List;
 
 public class CustomerOrdersPageController implements ControllerInterface {
@@ -24,11 +29,28 @@ public class CustomerOrdersPageController implements ControllerInterface {
     Button backButton;
 
     @FXML
-    TextArea ordersListArea;
+    TextField searchBar;
+
+    @FXML
+    TableView<Order> tableOrders;
+
+    @FXML
+    TableColumn<Order, LocalDate> tableDataZamowienia;
+
+    @FXML
+    TableColumn<Order, String> tableIdKlienta;
+
+    @FXML
+    TableColumn<Order, String> tableIdZamowienia;
+
+    @FXML
+    TableColumn<Order, String> tableStatusZamowienia;
 
     private String customerLogin = "";
 
     private final OrderZapytania orderZapytania = OrderZapytania.getInstance();
+
+    private final ObservableList<Order> ordersObservableList = FXCollections.observableArrayList();
 
     public void loadOrders() {
         System.out.println("customerLogin: " + customerLogin);
@@ -36,20 +58,50 @@ public class CustomerOrdersPageController implements ControllerInterface {
         try {
             List<Order> orders = orderZapytania.getOrdersByNick(customerLogin);
 
-            StringBuilder ordersList = new StringBuilder();
+            ordersObservableList.addAll(orders);
 
-            orders.forEach(order -> {
-                ordersList.append(order.toString()).append("\n");
+            tableDataZamowienia.setCellValueFactory(new PropertyValueFactory<>("dataZamowienia"));
+            tableIdKlienta.setCellValueFactory(new PropertyValueFactory<>("idKlienta"));
+            tableIdZamowienia.setCellValueFactory(new PropertyValueFactory<>("idZamowienia"));
+            tableStatusZamowienia.setCellValueFactory(new PropertyValueFactory<>("statusZamowienia"));
+
+            tableOrders.setItems(ordersObservableList);
+
+            FilteredList<Order> filteredData = new FilteredList<>(ordersObservableList, b -> true);
+
+            searchBar.textProperty().addListener((observable, oldValue, newValue) -> {
+                filteredData.setPredicate(order -> {
+
+                    if (newValue == null || newValue.isEmpty() || newValue.isBlank()) {
+                        return true;
+                    }
+
+                    String lowerCaseFilter = newValue.toLowerCase();
+
+                    if (order.getIdZamowienia().toLowerCase().contains(lowerCaseFilter)) {
+                        return true;
+                    } else if (order.getIdKlienta().toLowerCase().contains(lowerCaseFilter)) {
+                        return true;
+                    } else if (order.getStatusZamowienia().toLowerCase().contains(lowerCaseFilter)) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                });
             });
 
-            ordersListArea.setText(ordersList.toString());
+            SortedList<Order> sortedData = new SortedList<>(filteredData);
+            sortedData.comparatorProperty().bind(tableOrders.comparatorProperty());
+
+            tableOrders.setItems(sortedData);
+
 
         } catch (Exception e) {
             System.out.println("Error while getting orders list: " + e.getMessage());
         }
     }
 
-    public void back(ActionEvent actionEvent){
+    public void back(ActionEvent actionEvent) {
         System.out.println("back");
 
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/application/customer/customer-page.fxml"));
